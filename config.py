f614b8c08660f35abb77ddea0a4826d35b10d9bd
LordNani <rigansu@gmail.com>
2021-10-25 06:24:15 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+HEADER_URL = "https://cdn.cloudflare.steamstatic.com/steam/apps/{}/header.jpg"
+STEAM_ID_URL = "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key={}&vanityurl={}"
+OWNED_GAMES_URL = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={}&steamid={}&include_appinfo=true&include_played_free_games=true&include_free_sub=true"
+
+DATA_PATH = "data.xlsx"
+IMG_PATH = "img/"
---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+from config import HEADER_URL, IMG_PATH
+
+def save_header(app_id, app_name):
+    response = requests.get(HEADER_URL.format(app_id))
+
+    file = open("{}{}.jpg".format(IMG_PATH, app_name), "wb")
+    file.write(response.content)
+    file.close()
+
+
+def main():
+    #get steam username from console
+    #get steam id from username (Resolve Vanity)
+    #get owned games using steam id
+    #open a data.xlsx
+    #if game in list =>
+    #   add username to cell containg the owners
+    #   stop
+    #else =>
+    #   get info from steamspy
+    #   get info from steam api
+    #   save header img
+    #   save all data in data.xlsx
+    #   save owner
+    #   stop
+    save_header(220, "half-life-2")
+
+
+if __name__ == '__main__':
+    main()
